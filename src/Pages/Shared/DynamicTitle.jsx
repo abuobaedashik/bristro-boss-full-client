bdacd2a7413f3460082cc2358b1ac2794445c6ec
abuobaedashik <abuobaedashik@gmail.com>
2025-01-14 10:51:30 -0800

    clone project push done

--- src/Pages/Shared/DynamicTitle.jsx
+++ src/Pages/Shared/DynamicTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DynamicTitle = ({subheading,heading}) => {
     return (
-        <div className='md:w-4/12 mx-auto text-center '>
+        <div className='md:w-4/12 mx-auto text-center mt-2 '>
             <h1 className='text-base font-normal text-[#d99904] mb-2 '>{subheading}</h1>
             <h1 className='text-4xl font-bold border-y-4 py-4 border-[#E8E8E8]  text-[#151515]'>{heading}</h1>
         </div>
