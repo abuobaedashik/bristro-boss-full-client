bf819838b77033f9b5018750e5aee89e9c16f1e8
abuobaedashik <abuobaedashik@gmail.com>
2025-02-16 19:19:25 +0600

    item add and delete from api successfully

--- src/Pages/Dashboard/Dashboard Pages/Admin Page/ManageItems.jsx
+++ src/Pages/Dashboard/Dashboard Pages/Admin Page/ManageItems.jsx
@@ -10,25 +10,35 @@ const ManageItems = () => {
   const axiosPublic = useAxiosPublic();
   const axiosSecure = useAxiosSecure();
 
-  const [menu, loadar] = useMenu();
-    const handleDelete = async (id) => {
+  const [menu, loadar,refetch] = useMenu();
+  const handleDeleteMenu = async (item) => {
     //   const res = await axiosSecure.delete(`/menu/${id}`);
     Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
-      }).then( async(result) => {
-        if (result.isConfirmed) {
-            const res = await axiosSecure.delete(`/menu/${id}`);
-            console.log(res.data);
-        }
-      });
-    
-    }
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await axiosSecure.delete(`/menu/${item._id}`)
+
+          .then((res) => {
+                   if (res.data.deletedCount > 0) {
+                refetch();
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "Your file has been deleted.",
+                  icon: "success",
+                });
+                console.log(res.data);
+            }
+          });
+      }
+    });
+  };
   console.log("menu iten from managege itm ", menu);
   return (
     <div>
@@ -41,6 +51,7 @@ const ManageItems = () => {
 
       {/* table all menu item */}
       <div className="mt-6 mx-8">
+          <div className="my-3 text-base font-bold">Total items : {menu?.length}</div>
         <div className="overflow-x-auto">
           <table className="table table-sm h-[300px] table-pin-rows table-pin-cols">
             <thead className="bg-[#d1a054] py-4  text-base font-semibold">
@@ -67,14 +78,14 @@ const ManageItems = () => {
                       </div>
                     </div>
                   </td>
+                  <td>{item.name}</td>
                   <td>{item.price}</td>
-                  <td>Littel, Schaden and Vandervort</td>
-                  <td>Canada</td>
+                  <td>Update</td>
                   <td>
                     {" "}
                     <p className="">
                       <button
-                        onClick={() => handleDelete(item._id)}
+                        onClick={() => handleDeleteMenu(item)}
                         className="btn hover:text-[#131313] btn-ghost btn-xs bg-red-600 px-2 py-1 text-[#ffffff]"
                       >
                         <FaTrashAlt></FaTrashAlt>
